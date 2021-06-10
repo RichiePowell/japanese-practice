07409f69224e6f79566bd0d5fb972581d215546d
Rich Powell <rich@intech.ventures>
2021-06-10 22:55:32 +0100

    Fix answer sizing bug

--- src/components/InputChoices.js
+++ src/components/InputChoices.js
@@ -10,14 +10,14 @@ const setContainsWord = (set) => {
 const InputChoices = () =>
   <Consumer>
     { ({ answerOptions, actions }) => (
-      <div className="answer-options">
+      <div className={ `answer-options${ setContainsWord(answerOptions) ? ' answer-options--contains-word' : '' }` }>
         {answerOptions && answerOptions.map( (answers, index) => {
           let answer = answers[0];
           return (
           <button
             key={ answer + index }
             type="button"
-            className={ "answer-options__option" + (setContainsWord(answerOptions) ? ' answer-options__option--word' : '') }
+            className={ "answer-options__option" }
             name="answer"
             value={ answer }
             onClick={ () => actions.checkAnswer(answer) }
